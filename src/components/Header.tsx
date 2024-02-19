3e0c4e2d222724b965d8aa184eb5df89302809a7
Kovtunets Mykola <kovtunets73@gmail.com>
2024-02-19 12:27:18 +0100

    transition header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = ({ children, full }: Props) => {
     <header
       className={`sticky ${
         full ? "min-w-[100%]" : "min-w-[280px]"
-      } py-6 top-0 bg-base-100`}
+      } py-6 top-0 bg-gradient-to-b from-base-100 via-base-100 via-95% to-transparent`}
     >
       {children}
     </header>
